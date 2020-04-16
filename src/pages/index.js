411716cd29844a4ea11896f6bc656ef956c28fa9
Davy39 <cottet_davy@yahoo.fr>
2020-04-16 23:55:04 +0200

    Correction orthographe

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,7 +24,7 @@ const IndexPage = ({
         {!site.siteMetadata.w3l_dom_key ? null : <meta name="w3l-domain-verification" content={site.siteMetadata.w3l_dom_key} />}
       </Helmet>
       <HeroHeader/>
-      <h2>Billet de Blog &darr;</h2>
+      <h2>Billets de Blog &darr;</h2>
       <div className="grids">
         {Posts}
       </div>
